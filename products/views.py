720dba0a9621e92e853593984cc38805463c92a7
Simone Bellavia <simonebellavia@Simones-MBP.fritz.box>
2022-02-17 15:55:14 +0100

    added UserAPIView Class, modified database

--- products/views.py
+++ products/views.py
@@ -1,8 +1,11 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from .models import Product
+from .models import Product, User
 from .serializers import ProductSerializer
+import random
+
 
 class ProductViewSet(viewsets.ViewSet):
     def list(self, request): #/api/products
@@ -32,3 +35,11 @@ class ProductViewSet(viewsets.ViewSet):
         product = Product.objects.get(id=pk)
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+class UserAPIView(APIView):
+    def get(self, _):
+        users = User.objects.all()
+        user = random.choice(users)
+        return Response({
+            'id': user.id
+        })
